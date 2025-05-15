a97e41c9aa73688852f30d5b59789e0d7c9deb65
bsrat <bsratwellerufael2023@gmail.com>
2025-05-15 12:30:02 +0300

    qrcode is added

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,6 +3,10 @@ import { Inject } from "@nestjs/common";
 import { Client,Transport,ClientProxy } from "@nestjs/microservices";
 import { ProductDto } from "src/dto/create-product.dto";
 import { UpdateProductDto } from "src/dto/update-product.dto";
+import { firstValueFrom } from "rxjs";
+import { Response } from "express";
+
+import { Res } from "@nestjs/common";
 @Controller('product')
 export class ProductController {
   constructor(
@@ -35,17 +39,30 @@ export class ProductController {
   async getProduct(@Param('id') id: number) {
     return this.client.send('get-one-product', id);
   }
-  
+
   @Delete('/:productId')
   async deleteProduct(@Param('productId') productId: number) {
     return this.client.send('product-deleted', +productId);
   }
   @Patch(':id')
-async updateProduct(
-  @Param('id') productId: number,
-  @Body() updateData: UpdateProductDto,
-) {
-  return this.client.send('product-updated', { productId, updateData });
-}
+  async updateProduct(
+    @Param('id') productId: number,
+    @Body() updateData: UpdateProductDto,
+  ) {
+    return this.client.send('product-updated', { productId, updateData });
+  }
+  @Get(':id/qrcode')
+  async getQRCode(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const qrDataUrl: string = await firstValueFrom(
+        this.client.send('generate_qr', parseInt(id)),
+      );
 
+      const imgBuffer = Buffer.from(qrDataUrl.split(',')[1], 'base64');
+      res.setHeader('Content-Type', 'image/png');
+      res.send(imgBuffer);
+    } catch (err) {
+      res.status(404).send('Product not found or QR generation failed');
+    }
+  }
 }
